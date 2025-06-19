79b3e92685557c0b04ca14570eed8d4ca95f4396
Daniel <dq1502584@gmail.com>
2025-06-18 21:31:41 -0500

    Add files via upload

---
+++ trabajos de javascript/semana 1 - js/taller 1/main.js
@@ -0,0 +1,4 @@
+let nombre = prompt("ingrese su nombre")
+let edad = Number(prompt("ingrese su edad"));
+let ciudad = prompt("ingrese su ciudad")
+console.log(`Hola, ${nombre} tienes ${edad} y vives en ${ciudad}`)
---
+++ trabajos de javascript/semana 1 - js/taller 2/main.js
@@ -0,0 +1,20 @@
+let a = Number(prompt("Escribe el primer lado del triangulo"));
+let b = Number(prompt("Escribe el segundo lado del triangulo"));
+let c = Number(prompt("Escribe el tercer lado del triangulo"));
+
+function clasificar(a,b,c) {
+    if (a + b <= c || a + c <= b || b + c <= a) {
+        console.log ("no es un triangulo");
+    } 
+   else if (a === c && b === c){
+        console.log ("equilatero");
+    }
+    else if (a === b || a === c || b === c) {
+        console.log ("isoceles");
+    }
+    else 
+   console.log("escaleno"); 
+
+    }
+ 
+    clasificar(a,b,c)
